618a7e1e84720e48f1eb98f49e0654ef0d6623aa
pibico <pibico.sl@gmail.com>
2021-01-20 18:18:00 +0100

    modified:   project_controls/fixtures/custom_field.json 	modified:   project_controls/fixtures/custom_script.json 	modified:   project_controls/fixtures/report.json 	modified:   project_controls/fixtures/translation.json 	modified:   project_controls/project_controls/custom.py 	new file:   project_controls/project_controls/doctype/word_template/__init__.py 	new file:   project_controls/project_controls/doctype/word_template/test_word_template.py 	new file:   project_controls/project_controls/doctype/word_template/word_template.js 	new file:   project_controls/project_controls/doctype/word_template/word_template.json 	new file:   project_controls/project_controls/doctype/word_template/word_template.py 	modified:   requirements.txt

--- project_controls/project_controls/custom.py
+++ project_controls/project_controls/custom.py
@@ -38,3 +38,51 @@ def get_emplid(user):
   """ Get from database employee id from user logged in """
   data = frappe.db.sql("""SELECT * from `tabEmployee` WHERE user_id=%s""", user, True)
   return data
+
+from six import BytesIO
+from docxtpl import DocxTemplate
+
+def _fill_template(template, data):
+    """
+    Fill a word template with data.
+
+    Makes use of BytesIO to write the resulting file to memory instead of disk.
+
+    :param template:    path to docx file or file-like object
+    :param data:    dict with keys and values
+    """
+    doc = DocxTemplate(template)
+    doc.render(data)
+    _file = BytesIO()
+    doc.docx.save(_file)
+    return _file
+
+@frappe.whitelist()
+def fill_and_attach_template(doctype, name, template):
+    """
+    Use a documents data to fill a docx template and attach the result.
+
+    Reads a document and a template file, fills the template with data from the
+    document and attaches the resulting file to the document.
+
+    :param doctype"     data doctype
+    :param name"        data name
+    :param template"    name of the template file
+    """
+    data = frappe.get_doc(doctype, name)
+    data_dict = data.as_dict()
+
+    template_doc = frappe.get_doc("File", template)
+    #frappe.msgprint(template_doc.get_full_path())
+    
+    template_path = template_doc.get_full_path()
+
+    output_file = _fill_template(template_path, data_dict)
+    output_doc = frappe.get_doc({
+        "doctype": "File",
+        "file_name": "-".join([name, template_doc.file_name]),
+        "attached_to_doctype": doctype,
+        "attached_to_name": name,
+        "content": output_file.getvalue(),
+    })
+    output_doc.save()
